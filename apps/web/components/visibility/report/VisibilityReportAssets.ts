f847aa68ce308fda8a4895b0c69a6c2890464582
Walklandstudio <lisa@studiods.co.za>
2026-04-17 13:41:16 +0000

    change icons for vl report

--- apps/web/components/visibility/report/VisibilityReportAssets.ts
+++ apps/web/components/visibility/report/VisibilityReportAssets.ts
@@ -2,19 +2,19 @@
 
 export const VISIBILITY_REPORT_ASSETS = {
   insights: {
-    marketReality: "/images/visibility-report/icons/market-reality.svg",
-    opportunity: "/images/visibility-report/icons/opportunity.svg",
-    nextMove: "/images/visibility-report/icons/next-move.svg",
+    marketReality: "/images/visibility-report/icons/market-reality.png",
+    opportunity: "/images/visibility-report/icons/opportunity.png",
+    nextMove: "/images/visibility-report/icons/next-move.png",
   },
   sections: {
-    welcome: "/images/visibility-report/icons/welcome.svg",
-    howToUse: "/images/visibility-report/icons/how-to-use.svg",
-    understanding: "/images/visibility-report/icons/understanding.svg",
-    strengths: "/images/visibility-report/icons/strengths.svg",
-    friction: "/images/visibility-report/icons/friction.svg",
-    signalGraph: "/images/visibility-report/icons/signal-graph.svg",
-    coaching: "/images/visibility-report/icons/coaching.svg",
-    strategy: "/images/visibility-report/icons/strategy.svg",
+    welcome: "/images/visibility-report/icons/welcome.png",
+    howToUse: "/images/visibility-report/icons/how-to-use.png",
+    understanding: "/images/visibility-report/icons/understanding.png",
+    strengths: "/images/visibility-report/icons/strengths.png",
+    friction: "/images/visibility-report/icons/friction.png",
+    signalGraph: "/images/visibility-report/icons/signal-graph.png",
+    coaching: "/images/visibility-report/icons/coaching.png",
+    strategy: "/images/visibility-report/icons/strategy.png",
   },
   people: {
     bogdan: "/images/visibility-report/people/bogdan-stan.png",
